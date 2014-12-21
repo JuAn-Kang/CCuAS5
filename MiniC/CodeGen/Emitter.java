859a8448820270e15f44f710a4cc818e834ae2c0
erynet <ldh668@gmail.com>
2014-12-22 08:03:25 +0900

    attempt to fix import error in Emitter.java (2)

--- MiniC/CodeGen/Emitter.java
+++ MiniC/CodeGen/Emitter.java
@@ -4,8 +4,9 @@ import java.io.*;
 import MiniC.AstGen.*;
 import MiniC.StdEnvironment;
 import MiniC.ErrorReporter;
-import MiniC.CodeGen.Frame;
-import MiniC.CodeGen.JVM;
+//import MiniC.CodeGen.Frame;
+//import MiniC.CodeGen.JVM;
+import MiniC.CodeGen.*;
 
 public class Emitter implements Visitor {
 
